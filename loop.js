88a0629478b16d311aec02fc6a842f501e4a5fb7
Lia Va <mistickusya.2012@gmail.com>
2021-08-25 04:22:39 +0300

    fix: more fault tolerance!

--- loop.js
+++ loop.js
@@ -1,7 +1,11 @@
 module.exports = async (fn, interval) => {
-    const loop = async () => (
-        await fn(),
-        setTimeout(loop, interval)
-    );
+    const loop = async () => {
+        try {
+            await fn();
+        } catch(e) {
+            console.log(e);
+        }
+        setTimeout(loop, interval);
+    };
     await loop();
 }
