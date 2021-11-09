ffacbc0b0c53676eee59c743fee02e0273b00190
Lia Va <33329419+MiloLug@users.noreply.github.com>
2021-11-09 03:01:34 +0200

    a

--- repos-updater.js
+++ repos-updater.js
@@ -6,8 +6,8 @@ const utils = require('./utils.js');
 const { GITHUB_TOKEN } = require('./config.json');
 
 
-async function getFilesPage(page) {
-    const queryString = `https://api.github.com/search/code?page=${page}&per_page=100&q=${encodeURIComponent('extension:spwn')}`;
+async function getFilesPage(page, q='') {
+    const queryString = `https://api.github.com/search/code?page=${page}&per_page=100&q=${encodeURIComponent(q)}`;
     return (await fetch(
         queryString, {
             headers: {
@@ -24,21 +24,55 @@ module.exports = bot => async () => {
     
     // collect the repos
     let p = 1;
+    let offset = 0;
+    let emptyCount = 0;
+    let errorsCount = 0;
+    let flip = 0;
+    
     while(true) {
-        try{
-            const response = await getFilesPage(p);
-            if(!response.items.length)
+        if(emptyCount > 2 || p > 10 || errorsCount > 4){
+            if(offset > 51)
                 break;
+            
+            p = 1;
+            offset++;
+            emptyCount = 0;
+            errorsCount = 0;
+            continue;
+        }
+        
+        await sleep(5000 + errorsCount*20000); // to avoid rate-limiting
+        
+        try{
+            const response = await getFilesPage(
+                p,
+                'extension:spwn'
+                + ' size:' + (offset > 50 ? '>=500' : offset*10 + '..' + (offset+1)*10)
+                + 'sort:updated-' + ['asc', 'desc'][flip % 2]
+            );
+            if(!response?.items?.length) {
+                emptyCount++;
+                continue;
+            }
             
             for(const file of response.items) {
                 if(!repos[file.repository.id]) {
                     repos[file.repository.id] = file.repository;
                 }
             }
-            p++;
+            
+            errorsCount = 0;
+            emptyCount = 0;
+            flip++;
+            
+            if(flip > 1){
+                flip = 0;
+                p++;
+            }
         }
-        catch(e){}
-        await sleep(5000); // to avoid rate-limiting
+        catch(e){
+            errorsCount++;
+        }
     }
     
     const dbRepos = await db.getAllRepos(['id']);
